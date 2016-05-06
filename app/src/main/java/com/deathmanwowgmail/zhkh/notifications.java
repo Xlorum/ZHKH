54522cfcbc3643244e7b9c4ca891b0dbed1c4761
Xlorum <Deathmanwow@gmail.com>
2016-05-06 21:39:12 +0300

    xlorum

--- app/src/main/java/com/deathmanwowgmail/zhkh/notifications.java
+++ app/src/main/java/com/deathmanwowgmail/zhkh/notifications.java
@@ -10,6 +10,6 @@ public class notifications extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
         //3232323
-        //Сережа хоть и гей,но молодец.!!&& Серега,увидь эти изменения!
+        //Сережа хоть и гей,но молодец.!!&& Серега,увидь эти изменения!Ещё разок!
     }
 }
