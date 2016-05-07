fffe13fbe70c93c4052714da28bfdb9a13904539
yachlovek <yachlovek@gmail.com>
2016-05-07 20:44:00 +0300

    Добавлен слайдин Activity с помощью пальца

--- app/src/main/java/com/deathmanwowgmail/zhkh/notifications.java
+++ app/src/main/java/com/deathmanwowgmail/zhkh/notifications.java
@@ -1,36 +1,72 @@
 package com.deathmanwowgmail.zhkh;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Button;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
-import android.content.Intent;
+import android.view.View.OnTouchListener;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+import android.widget.ViewFlipper;
 
-public class notifications extends AppCompatActivity implements View.OnClickListener {
+public class notifications extends Activity implements OnTouchListener
+{
+    //Объявление переменных
+    private ViewFlipper flipper = null;
+    private float fromPosition;
 
-    Button btn1;
-
+    //Создание Activity
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
 
-        btn1 = (Button) findViewById(R.id.btn1);
-        btn1.setOnClickListener(this);
-        //3232323
-        //Сережа хоть и гей,но молодец.!!&& Серега,увидь эти изменения!Ещё разок!
-        //Поддурживаю
+        LinearLayout mainLayout = (LinearLayout) findViewById(R.id.main_layout);
+        mainLayout.setOnTouchListener(this);
+
+        flipper = (ViewFlipper) findViewById(R.id.viewFlipper);
+
+        //Создание View и добавление их в ViewFlipper
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        int layouts[] = new int[]{ R.layout.activity_notifications, R.layout.activity_two };
+        for (int layout : layouts)
+            flipper.addView(inflater.inflate(layout, null));
     }
 
-    @Override
-    public void onClick(View v){
-        switch (v.getId()){
-            case R.id.btn1:
-                Intent intent = new Intent(this, ActivityTwo.class);
-                startActivity(intent);
+    //Слайдинг при косании
+    public boolean onTouch(View view, MotionEvent event)
+    {
+        switch (event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                fromPosition = event.getX();
                 break;
+            case MotionEvent.ACTION_UP:
+                float toPosition = event.getX();
+                if (fromPosition > toPosition)
+                {
+                    //Слайдинг вправо
+                    flipper.setInAnimation(AnimationUtils.loadAnimation(this,R.anim.go_next_in));
+                    flipper.setOutAnimation(AnimationUtils.loadAnimation(this,R.anim.go_next_out));
+                    flipper.showNext();
+                }
+                else if (fromPosition < toPosition)
+                {
+                    //Слайдинг влево
+                    flipper.setInAnimation(AnimationUtils.loadAnimation(this,R.anim.go_prev_in));
+                    flipper.setOutAnimation(AnimationUtils.loadAnimation(this,R.anim.go_prev_out));
+                    flipper.showPrevious();
+                }
             default:
                 break;
         }
+        return true;
     }
+
+
+
 }
